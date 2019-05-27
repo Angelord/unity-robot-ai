51f7ebf71b7d0477e6455ff3a7638893f298d269
Angel Paunov Georgiev <Georgiev1997@gmail.com>
2019-05-27 15:07:41 +0300

    Add goal instanteation

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -7,13 +7,19 @@ namespace MrRob  {
 		
 		[SerializeField] private float spacing = 1.0f;
 		[SerializeField] private GameObject blockPrefab;
+		[SerializeField] private GameObject goalPrefab;
 
 		private int _width;
 		private int _length;
+		private Point _goalPosition;
 		//Treasure 
 		//Robot
 		//Tile[,]
 		//TileObject[,]
+
+		//public Treasure treasure { get; }
+		//public Robot robot { get; }
+		public Point goalPosition { get { return _goalPosition; } }
 
 		private void Start() {
 			Initialize(10, 10);
@@ -22,14 +28,16 @@ namespace MrRob  {
 		public void Initialize(int width, int length) {
 			this._width = width;
 			this._length = length;
+			_goalPosition = new Point(width - 1, length - 1);
 
 			for(int x = 0; x < width; x++) {
 				for(int y = 0; y < length; y++) {
-					GameObject newBlock = Instantiate(blockPrefab, this.transform);
-					newBlock.transform.position = MapToWorldPos(new Point(x, y));
+					Vector3 pos = MapToWorldPos(new Point(x, y));
+					GameObject newBlock = Instantiate(blockPrefab, pos, Quaternion.identity, this.transform);
 				}
 			}
 
+			Instantiate(goalPrefab, MapToWorldPos(_goalPosition), Quaternion.identity, this.transform);
 			//place robot
 			//place goal
 		}
